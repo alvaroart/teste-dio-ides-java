73c8e1e73c9b3e23b6dae8e3612898787e4f225c
alvaroart <alvaro.rosa1985@gmail.com>
2022-01-11 21:12:05 -0300

    primeiro commit do eclipse.

--- src/com/art/PrimeiroPrograma.java
+++ src/com/art/PrimeiroPrograma.java
@@ -1,12 +1,15 @@
 package com.art;
 
+import com.art.model.Gato;
+
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 2;
+		Gato gato = new Gato();
+		/*int a = 2;
 		int b = 3;
-		System.out.println("A Paz do Senhor!\n\n" + (a+b));
+		System.out.println("A Paz do Senhor!\n\n" + (a+b));*/
 
 	}
 
