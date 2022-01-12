16d5463edac5356f6332666e9237c939c59effa0
alvaroart <alvaro.rosa1985@gmail.com>
2022-01-11 21:50:34 -0300

    soma test

--- src/com/art/PrimeiroPrograma.java
+++ src/com/art/PrimeiroPrograma.java
@@ -7,7 +7,7 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Gato gato = new Gato();
-		/*int a = 2;
+		/*int a = 4;
 		int b = 3;
 		System.out.println("A Paz do Senhor!\n\n" + (a+b));*/
 
